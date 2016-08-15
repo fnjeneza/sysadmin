ac6985223dc493966dfbff8d0189a8aae6ed0d9b
fnjeneza <njenezamed@gmail.com>
2016-08-15 09:39:28 +0200

    install all plugins in ~/.vim

--- setup_env.sh
+++ setup_env.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 sudo apt-get install -y tmux tree python-flake8 python-pep8 vim 
 
-git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #download the vimrc.conf
 wget https://raw.githubusercontent.com/fnjeneza/sysadmin/master/vimrc.conf
 mv vimrc.conf ~/.vimrc
 #run Vundle
-vim +PluginInstall
+vim +PluginInstall +qall
 
 mv tmux.conf ~/.tmux.conf
