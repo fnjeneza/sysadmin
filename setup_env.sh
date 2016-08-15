e2ae8efb3658bc0f8cfdb9991bb99cb2a771534e
fnjeneza <njenezamed@gmail.com>
2016-08-15 09:30:16 +0200

    download vimrc.conf

--- setup_env.sh
+++ setup_env.sh
@@ -4,6 +4,8 @@ sudo apt-get install -y tmux tree python-flake8 python-pep8 vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 
+#download the vimrc.conf
+wget https://raw.githubusercontent.com/fnjeneza/sysadmin/master/vimrc.conf
 mv vimrc.conf ~/.vimrc
 #run Vundle
 vim +PluginInstall
