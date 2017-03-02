ec8cabd6392a211ca23046f94e3b85380af44dde
Francois Njeneza <njenezamed@gmail.com>
2017-03-02 17:29:22 +0000

    centos prerequisites

---
+++ ansible/developer/wip/01_centos6_prerequisites_install.sh
@@ -0,0 +1,3 @@
+yum groupinstall -y "development tools"
+yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
+yum install -y wget tar
