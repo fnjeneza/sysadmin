699d01db7449e20e25bc5c6dff5e8069e1673ee4
Francois Njeneza <21510421@unicaen.fr>
2017-04-19 20:16:05 +0200

    vim options

--- ansible/developer/wip/07_vim_install.sh
+++ ansible/developer/wip/07_vim_install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-BRANCH=v8.0.0398
-
 cd /tmp
-git clone -b $BRANCH  https://github.com/vim/vim.git
-cd vim
-./configure --prefix=$HOME/.local --with-features=huge --enable-python3interp
+wget https://github.com/vim/vim/archive/v8.0.0567.zip
+unzip v8.0.0567.zip
+cd vim-8.0.0567/
+./configure --prefix=$HOME/.local/
+	--with-features=huge \
+	--enable-python3interp=dynamic \
+	--enable-cscope \
+	--enable-gui=no \
+	--enable-fail-if-missing \
+
 make -j5 && make install
